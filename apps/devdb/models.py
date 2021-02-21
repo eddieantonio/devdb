e652e78f415e12eb5b81c531cf80948d7e96334d
Eddie Antonio Santos <eddieantonio@hey.com>
2021-02-20 22:00:35 -0700

    migrate to first name/last name

--- apps/devdb/models.py
+++ apps/devdb/models.py
@@ -2,8 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Developer(models.Model):
-    name = models.CharField(max_length=128)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
     joined = models.DateField()
+
+    @property
+    def name(self):
+        return f"{self.first_name} {self.last_name}"
 
     def __str__(self):
         return self.name
