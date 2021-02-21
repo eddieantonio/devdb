1255aace397ed87d570954b9da6928e31f7e756d
Eddie Antonio Santos <eddieantonio@hey.com>
2021-02-20 22:21:30 -0700

    Show first name/last name in index.html

--- apps/devdb/views.py
+++ apps/devdb/views.py
@@ -5,5 +5,5 @@ from .models import Developer
 # Create your views here.
 def index(request):
     # https://www.youtube.com/watch?v=Vhh_GeBPOhs
-    developers = Developer.objects.all()
+    developers = Developer.objects.all().order_by('last_name')
     return render(request, "devdb/index.html", {"developers": developers})
